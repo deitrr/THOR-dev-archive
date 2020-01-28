5ecff89499ed993595b7931afdbc6001255dcb48
Urs Schroffenegger <nab@lampshade.ch>
2020-01-28 15:09:34 +0100

    fixes and renames

--- src/headers/cudaDeviceMemory.h
+++ src/headers/cudaDeviceMemory.h
@@ -4,33 +4,54 @@
 
 // class to manage device memory, to take care of allocation and deallocation.
 template<typename T>
-class CudaDeviceMemory
+class cuda_device_memory
 {
 public:
-  CudaDeviceMemory()
+  cuda_device_memory()
   {
 
   };
 
-  CudaDeviceMemory(size_t size)
+  cuda_device_memory(size_t size)
   {
     allocate(size);
   }
   
-  ~CudaDeviceMemory()
+  ~cuda_device_memory()
+  {
+    deallocate();
+  };
+
+  void deallocate()
   {
     if (device_ptr != nullptr)
       {
 	cudaError_t ret = cudaFree(device_ptr);
 	if (ret != cudaSuccess)
 	  printf("CudaDeviceMemory: device free error\n");
-	   
+	device_ptr = nullptr;
+	size = 0;
       }
-  };
-
-  bool allocate(size_t size)
+  }
+  
+  bool allocate(size_t size_in)
   {
-    cudaError_t ret = cudaMalloc((void**)&device_ptr, size*sizeof(T));
+    if (device_ptr != nullptr)
+      {
+	deallocate();
+      }
+    cudaError_t ret = cudaMalloc((void**)&device_ptr, size_in*sizeof(T));
+
+    if (ret == cudaSuccess)
+      {
+	size = size_in;
+      }
+    else
+      {
+	size = 0;
+	device_ptr = nullptr;
+      }
+    
     return ret == cudaSuccess;
   };
 
@@ -44,24 +65,36 @@ public:
     return device_ptr;
   };
 
+  bool allocated()
+  {
+    return device_ptr != nullptr;
+  };
+
+  size_t get_size()
+  {
+    return size;
+  };
+
+  
   // zero out device memory
   bool zero()
   {
+    
    cudaError_t ret =  cudaMemset(device_ptr, 0, sizeof(T) * size);
    return ret == cudaSuccess;
   };
 
   // copy data from device to local array
-  bool fetch(std::unique_ptr<T[]> host_ptr)
+  bool fetch(std::unique_ptr<T[]>& host_ptr)
   {
-    cudaError_t ret = cudaMemcpy( *host_ptr, device_ptr, size * sizeof(T), cudaMemcpyDeviceToHost);
+    cudaError_t ret = cudaMemcpy( host_ptr.get(), device_ptr, size * sizeof(T), cudaMemcpyDeviceToHost);
     return ret == cudaSuccess;
   };
 
   // copy data from local array to device
-  bool put(std::unique_ptr<T[]> * host_ptr)
+  bool put(std::unique_ptr<T[]> &host_ptr)
   {
-    cudaError_t ret = cudaMemcpy(device_ptr, *host_ptr, size * sizeof(T), cudaMemcpyHostToDevice);
+    cudaError_t ret = cudaMemcpy(device_ptr, host_ptr.get(), size * sizeof(T), cudaMemcpyHostToDevice);
     return ret == cudaSuccess;
   };
 
